ffeca7fdc230ff2aa6a04ee23931ebe3686382da
Romi Kamin <romikamin1997@gmail.com>
2024-03-23 18:31:14 +0200

    provide fronend with backend uri from env variable

--- client/src/common.tsx
+++ client/src/common.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-const SERVER_ADDR = "http://localhost:5005/";
+const SERVER_ADDR = process.env.BACKEND_URI ?? "http://localhost:5005/";
 const GET_CODE_BLOCKS_ENDPOINT = "code-blocks";
 
 export {SERVER_ADDR, GET_CODE_BLOCKS_ENDPOINT};
