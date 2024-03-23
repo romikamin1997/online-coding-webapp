df7ad293a9d8f4f766b0bb5f8e4e4e05e4c14c9b
Romi Kamin <romikamin1997@gmail.com>
2024-03-23 21:49:34 +0200

    attempt to switch to xhr from axios

--- client/src/pages/Lobby.tsx
+++ client/src/pages/Lobby.tsx
@@ -3,10 +3,13 @@ import { Link } from "react-router-dom";
 import axios from 'axios';
 import { SERVER_ADDR, GET_CODE_BLOCKS_ENDPOINT } from '../common.tsx';
 
+function getData() {
+  
+}
 
 function Lobby() {
   const [codeBlocks, setCodeBlocks] = useState<Array<{title: string, code: string, solution: string}>>([]);
-
+  getData()
   // useEffect with no dependencies would let us have a similar behavior to
   // componentDidMount of a class component and only fetch the code block
   // titles and code fields once upon mounting
@@ -14,14 +17,31 @@ function Lobby() {
     // Sending a get request to the server to fetch all code block templates
     const endpoint = SERVER_ADDR + GET_CODE_BLOCKS_ENDPOINT
     console.debug(`Sending request to ${endpoint}`)
-    axios.get(SERVER_ADDR + GET_CODE_BLOCKS_ENDPOINT)
-      .then(function (response) {
-        // Update the current codeBlocks to the response retrieved from the server 
-        setCodeBlocks(response.data)
-      })
-      .catch(function (error) {
-        console.error(error)
-      });
+
+
+    // create a new XMLHttpRequest
+    var xhr = new XMLHttpRequest()
+
+    // get a callback when the server responds
+    xhr.addEventListener('load', () => {
+      // update the state of the component with the result here
+      console.log(JSON.parse(xhr.response))
+      setCodeBlocks(JSON.parse(xhr.response))
+    })
+    // open the request with the verb and the url
+    xhr.open('GET', SERVER_ADDR + GET_CODE_BLOCKS_ENDPOINT)
+    // send the request
+    xhr.send()
+
+
+    // axios.get(SERVER_ADDR + GET_CODE_BLOCKS_ENDPOINT)
+    //   .then(function (response) {
+    //     // Update the current codeBlocks to the response retrieved from the server 
+    //     setCodeBlocks(response.data)
+    //   })
+    //   .catch(function (error) {
+    //     console.error(error)
+    //   });
   }, [])
 
   return (
